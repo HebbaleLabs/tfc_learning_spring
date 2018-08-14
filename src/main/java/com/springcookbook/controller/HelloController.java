d683b11d37b8011ddd9118eacbd70d7e78ab3ce3
Shashank Shankar <shankar.shashank@gmail.com>
2018-08-14 17:10:07 +0530

    Quick-Fix for Home-Page

--- src/main/java/com/springcookbook/controller/HelloController.java
+++ src/main/java/com/springcookbook/controller/HelloController.java
@@ -5,12 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HomeController {
+public class HelloController {
+    @RequestMapping("hi")
+    @ResponseBody
+    public String hi() {
+        return "Hello, world.";
+    }
 
     @RequestMapping("/home")
-    @ResponseBody
     public String home() {
         return "home";
     }
-
 }
+
+
