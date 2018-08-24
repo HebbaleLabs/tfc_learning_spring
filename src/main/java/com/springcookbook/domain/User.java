4b26b2a65148574cab6c570ad049f3f72ea63653
Shashank Shankar <shankar.shashank@gmail.com>
2018-08-24 07:39:14 +0530

    Sync with course-content

--- src/main/java/com/springcookbook/domain/User.java
+++ src/main/java/com/springcookbook/domain/User.java
@@ -1,4 +1,47 @@
 package com.springcookbook.domain;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
 public class User {
+
+    private String firstName = "Fx";
+    private int age = 231;
+    private String country;
+    private boolean married;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public boolean getMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
+
 }
