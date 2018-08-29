3c55e516f50e21d3eb5a7ee3d6180a78e036d6cc
Shashank Shankar <shankar.shashank@gmail.com>
2018-08-29 11:12:03 +0530

    Baselined Starter-Project for Chapter-8 :- Using Aspect Oriented Programming

--- src/main/java/com/springcookbook/controller/UserController.java
+++ src/main/java/com/springcookbook/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/user")
 public class UserController {
 
 }
